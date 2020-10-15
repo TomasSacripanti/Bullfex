1260c76a54a688c377eb2bff7c05454ae9510d7b
Tomás Sacripanti <sacripantitomas@gmail.com>
2020-10-14 21:01:12 -0300

    Header finalizado

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,10 +1,34 @@
+//Imports
 import "../styles/styles.scss";
 import bullfexlogo from "../resources/images/logotipo-bullfex.svg";
 
+//Variables
+const hamburger = document.querySelector('.hamburger');
+const navList = document.querySelector('.nav-list');
+const items = document.querySelectorAll('.nav-item');
+
+
+//Event Listeners
 document.addEventListener('DOMContentLoaded', () => {
     let  logoContainer = document.querySelector('.img-container');
-    let bullfexImg = document.createElement('img');
-    bullfexImg.src = bullfexlogo;
-    bullfexImg.alt = 'Logotipo Bullfex';
-    logoContainer.appendChild(bullfexImg);
+    addImage(logoContainer, bullfexlogo, 'Logotipo Bullfex');
 });
+
+hamburger.addEventListener('click', displayNav);
+
+
+//Funciones
+
+function addImage(cont, img, msg) {
+    let imagen = document.createElement('img');
+    imagen.src = img;
+    imagen.alt = msg;
+    cont.appendChild(imagen);
+}
+
+function displayNav() {
+    navList.classList.toggle('open');
+    items.forEach(item => {
+        item.classList.toggle('fade');
+    })
+}
